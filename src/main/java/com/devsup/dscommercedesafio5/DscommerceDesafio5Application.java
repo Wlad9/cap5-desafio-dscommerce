d3edbb425038cbc799be7820ede341ad372b0625
wlad9 <zav8b9@gmail.com>
2023-12-06 18:41:20 -0300

    Projeto básico criado

---
+++ src/main/java/com/devsup/dscommercedesafio5/DscommerceDesafio5Application.java
@@ -0,0 +1,13 @@
+package com.devsup.dscommercedesafio5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DscommerceDesafio5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DscommerceDesafio5Application.class, args);
+	}
+
+}
